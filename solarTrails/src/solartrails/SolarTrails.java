7332ca6e58372f1b43c3d7414821fd0a5ffef343
Angela <angelamulberg@gmail.com>
2015-05-14 19:44:43 -0700

    5.14.15 - Version 1.0

---
+++ solarTrails/src/solartrails/SolarTrails.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solartrails;
+
+import byui.cit260.solarTrails.model.Player;
+
+/**
+ *
+ * @author amulberg
+ */
+public class SolarTrails {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Player playerOne = new Player();
+       
+       playerOne.setName("Fred Flintstone");
+       playerOne.setBestTime(7.00);
+       
+       String playerInfo = playerOne.toString();
+       System.out.println(playerInfo);
+       
+    }
+    
+}
