802506ad292ce921d802a1b53e435e514a57a700
User <User@192.168.1.67>
2015-05-16 23:01:41 -0500

    05/16/2015 Initial commit to repository

--- solarTrails/src/byui/cit260/solarTrails/model/UnidentifiedObject.java
+++ solarTrails/src/byui/cit260/solarTrails/model/UnidentifiedObject.java
@@ -6,6 +6,7 @@
 package byui.cit260.solarTrails.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
@@ -16,6 +17,11 @@ public class UnidentifiedObject implements Serializable {
     private String description;
     private String discoveries;
     private String coordinates;
+
+    public UnidentifiedObject() {
+    }
+    
+    
 
     public String getDescription() {
         return description;
@@ -40,6 +46,43 @@ public class UnidentifiedObject implements Serializable {
     public void setCoordinates(String coordinates) {
         this.coordinates = coordinates;
     }
+
+    @Override
+    public String toString() {
+        return "UnidentifiedObject{" + "description=" + description + ", discoveries=" + discoveries + ", coordinates=" + coordinates + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 43 * hash + Objects.hashCode(this.description);
+        hash = 43 * hash + Objects.hashCode(this.discoveries);
+        hash = 43 * hash + Objects.hashCode(this.coordinates);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final UnidentifiedObject other = (UnidentifiedObject) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.discoveries, other.discoveries)) {
+            return false;
+        }
+        if (!Objects.equals(this.coordinates, other.coordinates)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
     
     
