51ece872e0219be99e5640ce37b51ab71858f1eb
User <User@192.168.1.67>
2015-05-28 22:37:20 -0500

    05/16/2015 Initial commit to repository

---
+++ solarTrails/src/byui/cit260/solarTrails/model/RegularScene.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.solarTrails.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author montes-kemish
+ */
+public class RegularScene implements Serializable{
+    
+}
